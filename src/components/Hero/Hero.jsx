cc457ac60a6b030ab8d938b96677c0943711e49e
Marie <mariegunduz@gmail.com>
2024-06-09 22:53:08 +0300

    worked on Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,13 +4,16 @@ import Header from '../Header/Header'
 
 const Hero = () => {
   return (
-   <div className="hero">
-    <div className="left-h">
+    <div className="hero">
+      <div className="left-h">
+        <Header />
 
-        <Header/>
+      <div className="the-best-ad"></div>
+      <div></div>
+      <span>the best fitness club in the town</span>
+      </div>
+      <div className="right-h">right side</div>
     </div>
-    <div className="right-h">right side</div>
-   </div>
   )
 }
 
