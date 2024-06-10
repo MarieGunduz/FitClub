93a1b63678a7fd92a5949d7309df2caba8a0d471
Marie <mariegunduz@gmail.com>
2024-06-10 21:38:49 +0300

    modified Hero.css and Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,12 +7,22 @@ const Hero = () => {
     <div className="hero">
       <div className="left-h">
         <Header />
-
+        {/* the best ad */}
         <div className="the-best-ad">
-        <div></div>
+          <div></div>
           <span>the best fitness club in the town</span>
         </div>
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span>Shape </span>
+            <span>Your</span>
           </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+        </div>
+      </div>
       <div className="right-h">right side</div>
     </div>
   )
