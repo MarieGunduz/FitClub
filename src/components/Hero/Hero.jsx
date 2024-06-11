a73707dcc621b78aa34c2bb962db4a0380cf3799
Marie <mariegunduz@gmail.com>
2024-06-11 20:22:03 +0300

    working on buttons for gym website

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -44,6 +44,12 @@ const Hero = () => {
             <span>Fitness Programs</span>
             </div>
         </div>
+
+        {/* hero buttons */}
+        <div className="hero-buttons">
+          <buttons className="btn">Get Started</buttons>
+          <buttons className="btn">Learn More</buttons>
+        </div>
       </div>
       <div className="right-h">right side</div>
     </div>
