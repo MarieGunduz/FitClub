963691994d926581eb4f7f4443bfc2950fc07887
Marie <mariegunduz@gmail.com>
2024-06-12 22:06:55 +0300

    modified the right side site in Hero.jsx and styled the right side of site using Hero.css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -57,6 +57,11 @@ const Hero = () => {
       </div>
       <div className="right-h">
         <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span><span>116 bpm</span>
+        </div>
         </div>
     </div>
   )
