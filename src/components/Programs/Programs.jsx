9b51c0f12a5022f5dae5a28b46a7c9b224d7f3eb
Marie <mariegunduz@gmail.com>
2024-06-16 21:45:55 +0300

    modified Programs.jsx

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Programs.css";
 import {programsData} from "../../data/programsData";
+import RightArrow from "../..assets/rightArrow.png";
 
 const Programs = () => {
   return (
@@ -12,13 +13,16 @@ const Programs = () => {
         <span className="stroke-text">to shape you</span>
     </div>
 
-    <div className="programs-categories">
-        {programsData.map((program)==>
+    <div className="program-categories">
+        {programsData.map((program)=>(
         <div className="category">
             {program.image}
-            <span>{program.heading}</span><span>{program.heading}</span>
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now"><span>Join Now</span><img src={RightArrow} alt="" /></div>
+
         </div>
-    )}
+    ))}
     </div>
 
    </div>
