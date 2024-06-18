3c5b5f64dfa6fe899637dcbe716d52c5ed8e6d20
Marie <mariegunduz@gmail.com>
2024-06-18 23:21:01 +0300

    continued modfifed Reasons.jsx

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -4,6 +4,10 @@ import image1 from "../../assets/image1.png";
 import image2 from "../../assets/image2.png";
 import image3 from "../../assets/image3.png";
 import image4 from "../../assets/image4.png";
+import nb from "../../assets/nb.png";
+import adidas from "../../assets/adidas.png";
+import nike from "../../assets/nike.png";
+import tick from "../../assets/tick.png";
 
 const Reasons = () => {
   return (
