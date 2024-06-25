5180bf87ad593a9d93cd902f5b81c1cd46b18d97
Marie <mariegunduz@gmail.com>
2024-06-25 23:39:51 +0300

    modified Plans.jsx

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -5,13 +5,13 @@ const Plans = () => {
   return (
    <div className="plans-container">
 
-    <div className="programs-header">
-        <span>Ready To Start</span>
+    <div className="programs-header" style={{gap: '2rem'}}>
+        <span className="stroke-text">Ready To Start</span>
         <span>Your Journey</span>
-        <span>Now With Us</span>
+        <span className="stroke-text">Now WithUs</span>
     </div>
    </div>
-  )
-}
+  );
+};
 
 export default Plans
